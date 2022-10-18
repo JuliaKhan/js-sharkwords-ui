327cb87c14f54005df55cabc73644c0a4978e04c
“Julia <juliaabkhan@gmail.com>
2022-10-18 15:07:07 -0500

    added stylesheet and js to sharkwords.html and completed the functions in sharkwords.js
    Co-authored-by: lani lanjeylv@gmail.com

--- static/js/sharkwords.js
+++ static/js/sharkwords.js
@@ -20,13 +20,19 @@ const numWrong = 0;
 // Loop over the chars in `word` and create divs.
 // The divs should be appended to the section with id="word-container".
 const createDivsForChars = (word) => {
-  // Replace this with your code
+  const container = document.querySelector("#word-container");
+  for (const letter of word){
+    container.insertAdjacentHTML('beforeend', `<div class="letter-box ${letter}"></div>`);
+  }
 };
 
 // Loop over each letter in the alphabet and generate a button for each letter
 // The buttons should be appended to the section with id="letter-buttons"
 const generateLetterButtons = () => {
-  // Replace this with your code
+  const buttons = document.querySelector("#letter-buttons");
+  for (const letter of ALPHABET){
+    buttons.insertAdjacentHTML('beforeend', `<button>${letter}</button>`);
+  }
 };
 
 // Set the `disabled` property of `buttonEl` to `true.
@@ -34,7 +40,7 @@ const generateLetterButtons = () => {
 // `buttonEl` is an `HTMLElement` object.
 //
 const disableLetterButton = (buttonEl) => {
-  // Replace this with your code
+  buttonEl.setAttribute('disabled', true)
 };
 
 // This is a helper function we will use in the future
@@ -42,7 +48,12 @@ const disableLetterButton = (buttonEl) => {
 // For now, you should test it out to make sure it works
 
 const isLetterInWord = (letter) => {
-  // Replace this with your code
+  if (document.querySelector(`.letter-box ${letter}`)){
+    return true
+  }
+  else {
+    return false
+  }
 };
 
 // This is like if __name__ == '__main__' in Python
@@ -57,10 +68,10 @@ const isLetterInWord = (letter) => {
 
   // call the function that makes an empty line for each letter in the word
   // Replace this line with the function call
-
+  createDivsForChars(word);
   // call the function that makes a button for each letter in the alphabet
   // Replace this line with the function call
-
+  generateLetterButtons();
   // in the next lab, you will be adding functionality to handle when
   // someone clicks on a letter
 })();
